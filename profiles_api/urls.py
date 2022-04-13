d7f80a82ad2c0a7b4471b8e04f20fa3dc829d81b
Hyesu Lee <hyesulee2020@gmail.com>
2022-04-13 10:43:46 -0700

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,11 +6,12 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-# base name is needed if the viewset does not have queryset or want to override queryset name 
+# base name is needed if the viewset does not have queryset or want to override queryset name
 router.register('profile', views.UserProfileViewSet) # don't need base_name, because
 # User profileViewSet has queryset. REST framework could figure out from userprofile of the models
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
