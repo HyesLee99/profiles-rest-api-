acf06f009e7e68765334c417e991838dbb6ea2bc
Hyesu Lee <hyesulee2020@gmail.com>
2022-04-13 22:43:13 -0700

    added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -54,3 +55,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self): # recommend for all django bc it is good to specity how to represent "self"
         """Return string representation of our user"""
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """return the model as a string"""
+        return self.status_text
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -9,6 +9,7 @@ router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 # base name is needed if the viewset does not have queryset or want to override queryset name
 router.register('profile', views.UserProfileViewSet) # don't need base_name, because
 # User profileViewSet has queryset. REST framework could figure out from userprofile of the models
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
